73fc4feed0f0184fda10cdb2c60b5e87670048b0
Łukasz Piotr Staniszewski <l.staniszewski@stud.elka.pw.edu.pl>
2021-03-09 02:11:44 +0100

    modified gui

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-Minesweeper game made with pygame.
+# MINESWEEPER
+#### ŁUKASZ STANISZEWSKI
+<img src="https://user-images.githubusercontent.com/59453698/110401647-3f390900-807a-11eb-9bca-ef89c74dc35b.png" width=45%>
+
+Simple classic Minesweeper game made with python and pygame gui module.
+
+I. REQUIREMENTS:
++ PYTHON 3.8+
++ MODULES: **pygame** AND **numpy**
+II. HOW TO RUN:
++ DOWNLOAD REPO, UNZIP AND GO TO PROJECT'S FOLDER THROUG CONSOLE
++ RUN GAME WITH:
+```
+python ./run.py
+```
++ ENJOY!
+III. SOME SCREENSHOTS
++ Lost game:
+
+<img src="https://user-images.githubusercontent.com/59453698/110402532-d9e61780-807b-11eb-95cc-8522483737df.png" width=50%>
+
++ Won game:
+
+<img src="https://user-images.githubusercontent.com/59453698/110402798-4fea7e80-807c-11eb-82e1-2e6dccbc3265.png" width=50%>
 
 <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- SweeperGui.py
+++ SweeperGui.py
@@ -19,23 +19,26 @@ GAME_LEN = 5
 
 
 class SweeperGui:
-    def __init__(self, window_size=(720, 720), ic_dir="gui_files/bomb-24.png",
+    def __init__(self, window_size=(720, 720), ic_dir="gui_files/new-bomb.png",
                  caption="MINESWEEPER"):
         self.window_size = window_size
         self.screen = pg.display.set_mode(window_size)
         self.set_window_atributes(ic_dir, caption)
-        self.start = (self.window_size[0]/2 - (SIZE/2 * SQ_SIZE),
-                      self.window_size[1]/2 - (SIZE/2 * SQ_SIZE))
+        self.start = (self.window_size[0] / 2 - (SIZE / 2 * SQ_SIZE),
+                      self.window_size[1] / 2 - (SIZE / 2 * SQ_SIZE))
         self.digits = []
         for x in range(9):
             self.digits.append(pg.image.load(f"gui_files/digits/{x}.png"))
         self.flag_img = pg.image.load("gui_files/flag.png")
-        self.was_l_clicked = False
+        self.was_left_clicked = False
         self.was_right_clicked = False
         self.cl_dig = []
         for x in range(10):
             self.cl_dig.append(pg.image.load(f"gui_files/cl/{x}.png"))
         self.colon = pg.image.load(f"gui_files/cl/colon.png")
+        self.left_click = None
+        self.right_click = None
+        self.mouse_position = None
 
     def set_window_atributes(self, ic_dir, capt):
         pg.display.set_caption(capt)
@@ -53,14 +56,14 @@ class SweeperGui:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
-            m_pos = pg.mouse.get_pos()
-            l_click = pg.mouse.get_pressed()[0]
+            self.mouse_position = pg.mouse.get_pos()
+            self.left_click = pg.mouse.get_pressed()[0]
             full_size = SIZE * SQ_SIZE
-            if (self.start[0] <= m_pos[0] <= self.start[0] + full_size
-               and self.start[1] <= m_pos[1] <= self.start[1] + full_size):
-                if l_click == 1:
-                    info = self.game.l_click((m_pos[0] - self.start[0])//SIZE,
-                                             (m_pos[1] - self.start[1])//SIZE)
+            if (self.start[0] <= self.mouse_position[0] <= self.start[0] + full_size and
+                    self.start[1] <= self.mouse_position[1] <= self.start[1] + full_size):
+                if self.left_click == 1:
+                    info = self.game.l_click((self.mouse_position[0] - self.start[0]) // SIZE,
+                                             (self.mouse_position[1] - self.start[1]) // SIZE)
                     if info == 1:
                         self.show_bombs(False)
                         self.show_distances(True)
@@ -77,62 +80,64 @@ class SweeperGui:
                     pg.quit()
             if not self.game.finished:
                 self.show_clock()
-            m_pos = pg.mouse.get_pos()
-            l_click = pg.mouse.get_pressed()[0]
-            right_click = pg.mouse.get_pressed()[2]
-            full_size = SIZE * SQ_SIZE
-            if ((self.start[0] <= m_pos[0] <= self.start[0] + full_size)
-               and (self.start[1] <= m_pos[1] <= self.start[1] + full_size)):
-                if (l_click == 1 and not self.game.finished
-                   and not self.was_l_clicked):
-                    info = self.game.l_click((m_pos[0] - self.start[0])//SIZE,
-                                             (m_pos[1] - self.start[1])//SIZE)
-                    self.was_l_clicked = True
-                    if info == 1:
-                        self.show_distances()
-                    elif info == 2:
-                        self.show_bombs(True)
-                        self.game.finished = True
-                    elif info == 3:
-                        self.show_distances()
-                        self.show_bombs(False)
-                        self.game.finished = True
-                elif (right_click == 1 and not self.game.finished
-                      and not self.was_right_clicked):
-                    pos_x = int((m_pos[0] - self.start[0])//SIZE)
-                    pos_y = int((m_pos[1] - self.start[1])//SIZE)
-                    self.was_right_clicked = True
-                    if self.game.discovered_fields[pos_x][pos_y] == 0:
-                        self.screen.blit(self.flag_img,
-                                         (self.start[0] + pos_x * SQ_SIZE,
-                                          self.start[1] + pos_y * SQ_SIZE))
-                        self.game.discovered_fields[pos_x][pos_y] = 2
-                    elif self.game.discovered_fields[pos_x][pos_y] == 2:
-                        pg.draw.rect(self.screen, GREY_COLOR,
-                                     (self.start[0] + pos_x * SQ_SIZE,
-                                      self.start[1] + pos_y * SQ_SIZE,
-                                      SQ_SIZE, SQ_SIZE))
-                        pg.draw.rect(self.screen, DARK_GREY_COLOR,
-                                     (self.start[0] + pos_x * SQ_SIZE,
-                                      self.start[1] + pos_y * SQ_SIZE,
-                                      SQ_SIZE, SQ_SIZE), 1)
-                        self.game.discovered_fields[pos_x][pos_y] = 0
+            self.mouse_position = pg.mouse.get_pos()
+            self.left_click = pg.mouse.get_pressed()[0]
+            self.right_click = pg.mouse.get_pressed()[2]
+            self.perform_clicks()
             if self.Timer.isPassed():
                 self.show_bombs(True)
                 self.game.finished = True
-            rest_info = restButton.check_button(m_pos, l_click)
+            rest_info = restButton.check_button(self.mouse_position, self.left_click)
             if rest_info == 1:
                 self.restartGui()
             restButton.draw_button()
-            if l_click == 0:
-                self.was_l_clicked = False
-            if right_click == 0:
+            if self.left_click == 0:
+                self.was_left_clicked = False
+            if self.right_click == 0:
                 self.was_right_clicked = False
             pg.display.update()
 
+    def perform_clicks(self):
+        full_size = SIZE * SQ_SIZE
+        if ((self.start[0] <= self.mouse_position[0] <= self.start[0] + full_size)
+           and (self.start[1] <= self.mouse_position[1] <= self.start[1] + full_size)):
+            if (self.left_click == 1 and not self.game.finished and not self.was_left_clicked):
+                info = self.game.l_click((self.mouse_position[0] - self.start[0]) // SIZE,
+                                         (self.mouse_position[1] - self.start[1]) // SIZE)
+                self.was_left_clicked = True
+                if info == 1:
+                    self.show_distances()
+                elif info == 2:
+                    self.show_bombs(True)
+                    self.game.finished = True
+                elif info == 3:
+                    self.show_distances()
+                    self.show_bombs(False)
+                    self.game.finished = True
+            elif (self.right_click == 1 and not self.game.finished
+                  and not self.was_right_clicked):
+                pos_x = int((self.mouse_position[0] - self.start[0]) // SIZE)
+                pos_y = int((self.mouse_position[1] - self.start[1]) // SIZE)
+                self.was_right_clicked = True
+                if self.game.discovered_fields[pos_x][pos_y] == 0:
+                    self.screen.blit(self.flag_img,
+                                     (self.start[0] + pos_x * SQ_SIZE,
+                                      self.start[1] + pos_y * SQ_SIZE))
+                    self.game.discovered_fields[pos_x][pos_y] = 2
+                elif self.game.discovered_fields[pos_x][pos_y] == 2:
+                    pg.draw.rect(self.screen, GREY_COLOR,
+                                 (self.start[0] + pos_x * SQ_SIZE,
+                                  self.start[1] + pos_y * SQ_SIZE,
+                                  SQ_SIZE, SQ_SIZE))
+                    pg.draw.rect(self.screen, DARK_GREY_COLOR,
+                                 (self.start[0] + pos_x * SQ_SIZE,
+                                  self.start[1] + pos_y * SQ_SIZE,
+                                  SQ_SIZE, SQ_SIZE), 1)
+                    self.game.discovered_fields[pos_x][pos_y] = 0
+
     def draw_beg_board(self):
         pg.draw.rect(self.screen, GREY_COLOR, (self.start[0], self.start[1],
-                     SIZE * SQ_SIZE, SIZE * SQ_SIZE))
+                                               SIZE * SQ_SIZE, SIZE * SQ_SIZE))
         for x in range(SIZE):
             for y in range(SIZE):
                 pg.draw.rect(self.screen, DARK_GREY_COLOR,
@@ -183,7 +188,7 @@ class SweeperGui:
                         ind = int(self.game.distances[x][y])
                         self.screen.blit(self.digits[ind],
                                          (self.start[0] + x * SQ_SIZE,
-                                         self.start[1] + y * SQ_SIZE))
+                                          self.start[1] + y * SQ_SIZE))
 
     def show_clock(self):
         time_left = self.Timer.get_time_left()
@@ -205,4 +210,3 @@ class SweeperGui:
         self.draw_beg_board()
         self.game.restartGame()
         self.Timer.restartTimer()
-
